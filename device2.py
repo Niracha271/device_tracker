9ce75c2cf07870e6c5e768a4ec107d24d7721e81
Niracha271 <niracha.chirakanwutthikrai@g.swu.ac.th>
2025-11-22 16:45:50 +0700

    Update device2.py

--- device2.py
+++ device2.py
@@ -468,15 +468,6 @@ def menu_search(df: pd.DataFrame):
         return
 
     # ----- 2) ถ้าไม่เจอ → ค้นหาใน destroy_log -----
-def display_destroy_device_info(device: dict):
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.write(f"**Serial:** {device.get('Serial Number','-')}")
-    with col2:
-        st.write(f"**Device:** {device.get('Device Name','-')}")
-    with col3:
-        st.write(f"**Status:** 💥 Destroy")
-   
     client = get_google_sheets_client()
 
     try:
@@ -488,9 +479,6 @@ def display_destroy_device_info(device: dict):
             if str(row["Serial Number"]).upper() == search_serial.upper():
 
                 st.warning(f"💥 This device has been DESTROYED")
-                
-                # UI เหมือน device ปกติ
-                display_destroy_device_info(row)
 
                 st.write("---")
                 col1, col2 = st.columns(2)
@@ -782,6 +770,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
